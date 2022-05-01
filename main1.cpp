19b49e31223358db6bc359d9fd3481c332ef5fd5
Nahian Chowdhury <nahian00777>
2022-05-01 23:53:05 +0600

    created templates

---
+++ main1.cpp
@@ -0,0 +1,17 @@
+/* author: Nahian Chowdhury */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+typedef long long ll;
+
+signed main(void)
+{
+	// #ifndef ONLINE_JUDGE
+	// freopen("Error.txt", "w", stderr);
+	// #endif
+	fastio();
+	
+	
+}
---
+++ main2.cpp
@@ -0,0 +1,20 @@
+/* author: Nahian Chowdhury */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define debug(x) cerr << #x << " = " << x << "\n"
+
+signed main(void)
+{
+	// #ifndef ONLINE_JUDGE
+	// freopen("Error.txt", "w", stderr);
+	// #endif
+	fastio();
+	
+	
+}
