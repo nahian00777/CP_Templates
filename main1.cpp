5ad35be1c8b4dafbdbdd2fad919403fc57a79a68
Nahian Chowdhury <63234149+nahian00777@users.noreply.github.com>
2022-09-10 21:18:37 +0600

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,17 +1,12 @@
-/* author: Nahian Chowdhury */
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-
-typedef long long ll;
-
-signed main(void)
-{
-	// #ifndef ONLINE_JUDGE
-	// freopen("Error.txt", "w", stderr);
-	// #endif
-	fastio();
-	
-	
+#define print(arr, n) for(int i = 0;i < n; i++) cout << arr[i] << ' ';
+#define int long long
+ 
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  
+  return 0;
 }
